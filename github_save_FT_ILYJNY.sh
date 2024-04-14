db63f781ec38b6298802f39be8dc4ea8714c4611
ILYJNY <crimssel@gmail.com>
2024-04-14 21:30:59 +0900

    Sun Apr 14 21:30:59     2024 Up-to-Date

--- github_save_FT_ILYJNY.sh
+++ github_save_FT_ILYJNY.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git init
 git add .
+echo "Enter the commit(if you want automatically, type \"Auto\")"
 read input_commit
 if [ ${input_commit} = "Auto" ] ; then
   git commit -m "$(date) Up-to-Date"
