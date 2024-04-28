456f39068160db09f6d53cce5c378aeb6858cc73
ILYJNY <crimssel@gmail.com>
2024-04-28 17:32:08 +0900

    Add DFT and FFT

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include "Discrete Fourier Transforms/Discrete Fourier Transform/main.cpp"
 #include "main.h"
-
-
-
+#include "sstream"
+#include "Fast Fourier Transform/FFT.cpp"
+#include "Discrete Fourier Transforms/Discrete Fourier Transform/DFT.cpp"
+#define lf long double
 
 
 
@@ -33,9 +34,51 @@
         }
 
         else {
-            if (todo_list == "DFT") {
-                DFT_main();
+            vector<string> stringdatas;
+            vector<complex<lf>> datas;
+            string file_location;
+            cout << "give the Location of File";
+            cin >> file_location;
+            string line;
+            ifstream file(file_location); // example.txt 파일을 연다. 없으면 생성.
+            if(file.is_open()){
+                cout << "-----------------------------\n";
+                while(getline(file, line)) {
+                    stringdatas.push_back(line);
+                }
+                cout << "-----------------------------\n";
+                file.close(); // 열었떤 파일을 닫는다.
+            } else {
+                cout << "Unable to open \n";
             }
+
+            //string -> complex
+            for (auto & stringdata : stringdatas) {
+                stringdata;
+                std::istringstream iss(stringdata);
+                double real, imag;
+                iss >> real >> imag;
+                datas.emplace_back(real, imag);
+
+
+            }
+
+
+            if (todo_list == "DFT") {
+                vector<complex<lf>> result_DFT = DFT(datas);
+            }
+            if (todo_list == "FFT") {
+                string answer;
+                cout << "invert or forward?" << endl;
+                cin >> answer;
+                if (answer == "invert")
+                    fft(datas, true);
+
+                else {
+                    fft(datas, false);
+                }
+            }
+
         }
     }
 }
